c2faae29096538ee7d1b2890a7ab2c6372c5db03
Taylor Facen <taylor.facen@gmail.com>
2020-04-20 00:21:12 -0500

    Get battle by id

--- client/src/containers/BattleRoom/index.js
+++ client/src/containers/BattleRoom/index.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+
+import battleService from '../../services/battleService';
 
 class BattleRoom extends Component {
+    state = {
+        battle: null
+    }
+
+    componentDidMount(){
+        const battleId = this.props.match.params.battleId.toUpperCase();
+
+        battleService.getBattle(battleId)
+        .then(battle => {
+            this.setState({
+                battle
+            })
+        })
+        .catch(error => console.log(error))
+
+
+    }
+
     render(){
-        return (
+        const { battle } = this.state;
+        return battle && (
             <div className = "BattleRoom">
-                Battle: {this.props.match.params.battleId}
+                Battle: {battle.name}
             </div>
         )
     }
--- client/src/services/battleService.js
+++ client/src/services/battleService.js
@@ -2,11 +2,11 @@ import axios from 'axios';
 
 export default {
     getBattle: async battleId => {
-        let res = await axios.get(`api/battles/${battleId}`);
+        let res = await axios.get(`/api/battles/${battleId}`);
         return res.data || {};
     },
     createBattle: async data => {
-        let res = await axios.post(`api/battles`, data);
+        let res = await axios.post(`/api/battles`, data);
         return res.data || {};
     }
 
