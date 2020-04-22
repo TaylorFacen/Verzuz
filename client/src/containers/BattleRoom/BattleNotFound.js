7e1f24d06782177c68f282103f2bbb2c604785d1
Taylor Facen <taylor.facen@gmail.com>
2020-04-22 09:53:07 -0500

    battle not found screen

--- client/src/containers/BattleRoom/BattleNotFound.js
+++ client/src/containers/BattleRoom/BattleNotFound.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Image, Button } from 'react-bootstrap';
+
+const Shuffle = require('../../images/shuffle.png');
 
 export default () => (
     <div className = "BattleNotFound">
-        Battle Not Found
+        <Image className = "hero" src = { Shuffle } />
+        <h3>Oops, looks like that battle doesn't exist.</h3>
+        <p>Why not create one?</p>
+        <Button className = "cta" href = "/create-battle">Create Battle</Button>
+        <p><a href = "/">Home</a></p>
     </div>
 )
